34b85ddd6a00abd56f439a381167ec2c2b447693
t.zhuravlev <waynaylfg@gmail.com>
2026-02-14 22:28:46 +0300

    initial engine skeleton

---
+++ WhispEngine/WhispEngine.cpp
@@ -0,0 +1,12 @@
+﻿// WhispEngine.cpp: определяет точку входа для приложения.
+//
+
+#include "WhispEngine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
