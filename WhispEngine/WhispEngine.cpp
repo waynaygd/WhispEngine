271b7a2b01c04369778144238ef232b353fb1058
t.zhuravlev <waynaylfg@gmail.com>
2026-02-27 20:43:30 +0300

    some stuff

--- WhispEngine/WhispEngine.cpp
+++ WhispEngine/WhispEngine.cpp
@@ -1,24 +1,6 @@
 ﻿#include "core/Application.h"
 #include <cstring>
 
-static RenderBackend ParseBackend(int argc, char** argv)
-{
-    RenderBackend backend = RenderBackend::DX12;
-
-    for (int i = 1; i < argc; ++i)
-    {
-        const char* a = argv[i];
-        if (std::strncmp(a, "--backend=", 10) == 0)
-        {
-            const char* v = a + 10;
-            if (std::strcmp(v, "dx12") == 0) backend = RenderBackend::DX12;
-            else if (std::strcmp(v, "vulkan") == 0 || std::strcmp(v, "vk") == 0) backend = RenderBackend::Vulkan;
-        }
-    }
-
-    return backend;
-}
-
 int main(int argc, char** argv)
 {
     Application app;
